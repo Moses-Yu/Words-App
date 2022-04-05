d63c9afa3a874112faeabaf71aa8c625a55f76d9
Moses-Yu <mosesyumss@gmail.com>
2022-04-05 12:56:58 +0900

    #2 firstpage, loginpage, quiz1page, quiz2page, quizresultpage, searchpage, selectbook, selectquiz html페이지 구현, w3schools.css 리서치용 자료

---
+++ ScreenHTML/nav.js
@@ -0,0 +1,1 @@
+document.write("<input type='checkbox' id='nav_open'><label for='nav_open'><img src='../images/menu_icon.png'></label><div class='left_nav'><svg width='350' height='160'><ellipse cx='175' cy='85' rx='140' ry='60' /></svg><div>Search</div><div>My Page</div><div>Quiz</div><div>Login</div></div>")
