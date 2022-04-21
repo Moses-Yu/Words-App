c5a09bb15a2c40f555e041a098185f2a6fb2a08f
Moses-Yu <mosesyumss@gmail.com>
2022-04-21 22:44:27 +0900

    #8 구동 성공

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,10 +1,18 @@
 const path = require("path");
-
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+ 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname + "/build"),
+    path: path.resolve(__dirname + "/build")
+  },
+  devServer: {
+    devMiddleware: {
+      index: "index.html"
+    },
+    static: path.resolve("./build"),
+    port: 3000
   },
   mode: "development",
   module: {
@@ -12,8 +20,23 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: "/node_modules",
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
-    ],
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
   },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ]
 };
