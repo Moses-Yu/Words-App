ee61967d5348c2535872f153e977df930716ae26
Moses-Yu <mosesyumss@gmail.com>
2022-06-08 17:13:40 +0900

    파일정리

--- node_server/index.js
+++ node_server/index.js
@@ -24,3 +24,14 @@ indexRouter(app);
 app.listen(port, () => {
   console.log(`Express app listening at port: ${port}`);
 });
+
+var router = express.Router();
+
+/*
+	'/' url로 접근했을 경우,
+	결과값으로 public에 있는 index.html을 반환함을 의미
+	여기서 index.html은 아까 위에서 우리가 배포가능한 형태로 바꾼 Vue!
+*/
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+})
